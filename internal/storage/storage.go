6fb305fe453eba2a84eec897f737d94af50123b7
Keloran <keloran@develbox.info>
2025-05-21 23:04:21 +0100

    hmm

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 	policymodel "github.com/1rp-pw/orchestrator/internal/policy"
 	"github.com/bugfixes/go-bugfixes/logs"
-	kafka "github.com/confluentinc/confluent-kafka-go/v2/kafka"
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 	"github.com/google/uuid"
 	ConfigBuilder "github.com/keloran/go-config"
 	"io"
