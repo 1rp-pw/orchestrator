e03da4b8b7c0bd880f053aa627fac7269224471e
Keloran <keloran@develbox.info>
2025-06-12 23:50:36 +0100

    feat(storage): refactor policy storage to use DB functions
    Replace direct SQL inserts and updates with stored procedures for
    creating, updating, and publishing policies to centralize logic in
    the database. Update LoadPolicy and ListPolicies to query new views
    and tables reflecting the refactored schema. This improves data
    consistency, simplifies Go code, and enables better version and draft
    management.

--- internal/policy/policy.go
+++ internal/policy/policy.go
@@ -3,14 +3,18 @@ package policy
 import "time"
 
 type Policy struct {
-	ID        string      `json:"id"`
-	Rule      string      `json:"rule"`
-	Data      interface{} `json:"data"`
-	Tests     interface{} `json:"tests"`
-	Version   string      `json:"version"`
-	CreatedAt time.Time   `json:"createdAt"`
-	UpdatedAt time.Time   `json:"updatedAt"`
-	DataModel interface{} `json:"schema"`
-	Name      string      `json:"name"`
-	IsDraft   bool        `json:"draft"`
+	PolicyID    string      `json:"id"`
+	BaseID      string      `json:"baseId"`
+	Rule        string      `json:"rule"`
+	Data        interface{} `json:"data"`
+	Tests       interface{} `json:"tests"`
+	Version     string      `json:"version"`
+	CreatedAt   time.Time   `json:"createdAt"`
+	UpdatedAt   time.Time   `json:"updatedAt"`
+	DataModel   interface{} `json:"schema"`
+	Name        string      `json:"name"`
+	IsDraft     bool        `json:"draft"`
+	Description string      `json:"description"`
+	DraftID     string      `json:"draftId"`
+	Status      string      `json:"status"`
 }
